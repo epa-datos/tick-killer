ded245a145736e3d7667b1c7ea6449a21f0fce85
Metuendus <josecarlos.corona@outlook.com>
2022-08-09 02:23:16 +0000

    add a post and put request

--- killer_tick/tick.py
+++ killer_tick/tick.py
@@ -1,7 +1,6 @@
 """
-Class module
+Tickspot API client class module
 """
-from datetime import datetime, timedelta
 import os
 import logging
 import requests
@@ -9,27 +8,56 @@ import pandas as pd
 
 
 class KillTick:
-    """General use class"""
+    """API Requests client"""
     BASE_URL = "https://www.tickspot.com/99327/api/v2/"
 
     def make_request(self, method, url, params=None, data=None):
         """request gen"""
-        response = requests.request(
-            method,
-            f"{self.BASE_URL}/{url}",
-            params=params,
-            json=data,
-            headers={'Authorization': f"Token token={os.getenv('TICKSPOT_API_KEY')}",
-                     'User-Agent': "MyCoolApp (jcorona@epa.digital)"}
-        )
+        if method == "GET":
+            all_entries = []
+            new_results = True
+            page = 1
 
-        response_body = {}
-        try:
-            response_body = response.json()
-        except ValueError:
-            logging.warning("Unexpected Response '%s' from '%s'",
-                            response.content, response.url)
-        return pd.DataFrame.from_dict(response_body)
+            while new_results:
+                if params is None:
+                    params = {'page': page}
+                else:
+                    params.update({'page': page})
+
+                response = requests.request(
+                    method,
+                    f"{self.BASE_URL}/{url}",
+                    params=params,
+                    json=data,
+                    headers={'Authorization': f"Token token={os.getenv('TICKSPOT_API_KEY')}",
+                             'User-Agent': "MyCoolApp (jcorona@epa.digital)"}
+                )
+                response_body = {}
+                try:
+                    response_body = response.json()
+                    new_results = len(response_body) > 0
+                    page += 1
+                    all_entries.extend(response_body)
+                except ValueError:
+                    logging.warning("Unexpected Response '%s' from '%s'",
+                                    response.content, response.url)
+            return pd.DataFrame.from_dict(all_entries)
+        else:
+            response = requests.request(
+                method,
+                f"{self.BASE_URL}/{url}",
+                params=params,
+                json=data,
+                headers={'Authorization': f"Token token={os.getenv('TICKSPOT_API_KEY')}",
+                         'User-Agent': "MyCoolApp (jcorona@epa.digital)"}
+            )
+            response_body = {}
+            try:
+                response_body = response.json()
+            except ValueError:
+                logging.warning("Unexpected Response '%s' from '%s'",
+                                response.content, response.url)
+            return response_body
 
     def get_clients(self):
         """Get tick clients"""
@@ -48,3 +76,11 @@ class KillTick:
         payload = {'start_date': start_date,
                    'end_date': end_date}
         return self.make_request(method="GET", url="entries.json", params=payload)
+
+    def create_entry(self, data):
+        """Post a tickspot entry"""
+        return self.make_request("POST", "entries.json", data=data)
+
+    def update_entry(self, entry_id, data):
+        """Update given entry"""
+        return self.make_request("PUT", f"entries/{entry_id}.json", data=data)
